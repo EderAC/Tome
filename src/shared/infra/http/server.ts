ac471ba368006a5cd589e6768fe94fb409c0bb60
Eder Chiquito <eder.almeida40@gmail.com>
2022-02-04 19:20:42 -0300

    added console.log

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -48,7 +48,7 @@ async function init() {
 
   server.applyMiddleware({ app });
   app.listen(process.env.PORT || 3333, () => {
-    console.log('Server started on port 3333');
+    console.log(`Server started on port ${process.env.PORT}`);
   });
 }
 
