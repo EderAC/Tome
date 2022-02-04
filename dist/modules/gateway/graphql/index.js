bb983203f893292f65aff62b7dea5b501da18611
Eder Chiquito <eder.almeida40@gmail.com>
2022-02-04 19:05:34 -0300

    deleted dist folder

--- dist/modules/gateway/graphql/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.schema = void 0;
-const type_graphql_1 = require("type-graphql");
-exports.schema = (0, type_graphql_1.buildSchemaSync)({
-    resolvers: [`${__dirname}/**/*.{resolver,mutation}.{js,ts}`],
-    emitSchemaFile: 'schema.gql',
-});
