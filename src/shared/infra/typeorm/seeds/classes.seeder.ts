42488105bd1b8e564a4be5a387f0912ea75394a7
Eder Chiquito <eder.almeida40@gmail.com>
2022-02-04 21:56:16 -0300

    modified classes seeder

--- src/shared/infra/typeorm/seeds/classes.seeder.ts
+++ src/shared/infra/typeorm/seeds/classes.seeder.ts
@@ -1,85 +1,86 @@
+/* eslint-disable prettier/prettier */
 export const ClassesSeed = [
   {
-    name: 'Bárbaro',
+    name: "Bárbaro",
     description:
-      'Um feroz guerreiro de origem primitiva que pode entrar em fúria durante uma batalha',
-    hitDice: 'd12',
-    primaryAbility: 'Força',
+      "Um feroz guerreiro de origem primitiva que pode entrar em fúria durante uma batalha",
+    hitDice: "d12",
+    primaryAbility: "Força",
   },
   {
-    name: 'Bardo',
+    name: "Bardo",
     description:
-      'Um místico inspirador que possui poderes que ecoam a música da criação',
-    hitDice: 'd8',
-    primaryAbility: 'Carisma',
+      "Um místico inspirador que possui poderes que ecoam a música da criação",
+    hitDice: "d8",
+    primaryAbility: "Carisma",
   },
   {
-    name: 'Bruxo',
+    name: "Bruxo",
     description:
-      'Um portador de magia derivada de barganha com uma entidade planar',
-    hitDice: 'd8',
-    primaryAbility: 'Carisma',
+      "Um portador de magia derivada de barganha com uma entidade planar",
+    hitDice: "d8",
+    primaryAbility: "Carisma",
   },
   {
-    name: 'Clérigo',
+    name: "Clérigo",
     description:
-      'Um campeão sacerdotal que empunha magia divina a serviço de um poder maior',
-    hitDice: 'd8',
-    primaryAbility: 'Sabedoria',
+      "Um campeão sacerdotal que empunha magia divina a serviço de um poder maior",
+    hitDice: "d8",
+    primaryAbility: "Sabedoria",
   },
   {
-    name: 'Druida',
+    name: "Druida",
     description:
-      'Um sacerdote da Crença Antiga, detentor dos poderes da natureza – luz da lua e crescimento das plantas, fogo e relâmpagos – e capaz de adotar formas animais',
-    hitDice: 'd8',
-    primaryAbility: 'Sabedoria',
+      "Um sacerdote da Crença Antiga, detentor dos poderes da natureza – luz da lua e crescimento das plantas, fogo e relâmpagos – e capaz de adotar formas animais",
+    hitDice: "d8",
+    primaryAbility: "Sabedoria",
   },
   {
-    name: 'Feiticeiro',
+    name: "Feiticeiro",
     description:
-      'Um conjurador que possui magia latente advinda de um dom ou linhagem',
-    hitDice: 'd6',
-    primaryAbility: 'Carisma',
+      "Um conjurador que possui magia latente advinda de um dom ou linhagem",
+    hitDice: "d6",
+    primaryAbility: "Carisma",
   },
   {
-    name: 'Guerreiro',
+    name: "Guerreiro",
     description:
-      'Um mestre do combate, perito em uma vasta gama de armas e armaduras',
-    hitDice: 'd10',
-    primaryAbility: 'Força ou Destreza',
+      "Um mestre do combate, perito em uma vasta gama de armas e armaduras",
+    hitDice: "d10",
+    primaryAbility: "Força ou Destreza",
   },
   {
-    name: 'Ladino',
+    name: "Ladino",
     description:
-      'Um trapaceiro que utiliza de furtividade e astúcia para sobrepujar os obstáculos e inimigos',
-    hitDice: 'd8',
-    primaryAbility: 'Destreza',
+      "Um trapaceiro que utiliza de furtividade e astúcia para sobrepujar os obstáculos e inimigos",
+    hitDice: "d8",
+    primaryAbility: "Destreza",
   },
   {
-    name: 'Mago',
+    name: "Mago",
     description:
-      'Um usuário de magia escolado, capaz de manipular as estruturas da realidade',
-    hitDice: 'd6',
-    primaryAbility: 'Inteligência',
+      "Um usuário de magia escolado, capaz de manipular as estruturas da realidade",
+    hitDice: "d6",
+    primaryAbility: "Inteligência",
   },
   {
-    name: 'Monge',
+    name: "Monge",
     description:
-      'Um mestre das artes marciais, utilizando o poder corporal para atingir a perfeição física e espiritual ',
-    hitDice: 'd8',
-    primaryAbility: 'Destreza & Sabedoria',
+      "Um mestre das artes marciais, utilizando o poder corporal para atingir a perfeição física e espiritual ",
+    hitDice: "d8",
+    primaryAbility: "Destreza & Sabedoria",
   },
   {
-    name: 'Paladino',
-    description: 'Um guerreiro divino vinculado a um juramento sagrado',
-    hitDice: 'd10',
-    primaryAbility: 'Força & Carisma',
+    name: "Paladino",
+    description: "Um guerreiro divino vinculado a um juramento sagrado",
+    hitDice: "d10",
+    primaryAbility: "Força & Carisma",
   },
   {
-    name: 'Patrulheiro',
+    name: "Patrulheiro",
     description:
-      'Um guerreiro que utiliza de poderio marcial e magia natural para combater ameaças nos limites da civilização',
-    hitDice: 'd10',
-    primaryAbility: 'Destreza & Sabedoria',
+      "Um guerreiro que utiliza de poderio marcial e magia natural para combater ameaças nos limites da civilização",
+    hitDice: "d10",
+    primaryAbility: "Destreza & Sabedoria",
   },
 ];
